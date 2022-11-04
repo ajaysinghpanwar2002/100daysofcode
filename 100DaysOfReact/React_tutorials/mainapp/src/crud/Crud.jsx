e7265aef2d0c5b76662f9b9796cf1e416e60fe35
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-04 06:32:48 +0530

    3 november

--- 100DaysOfReact/React_tutorials/mainapp/src/crud/Crud.jsx
+++ 100DaysOfReact/React_tutorials/mainapp/src/crud/Crud.jsx
@@ -2,37 +2,45 @@ import React, { useState } from 'react'
 
 function Crud() {
 
-    const [todolist,setTodoList] = useState([]);
-    const [newTask,setNewTask] = useState("");
+    const [todolist, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
 
     // jaise he change hoga he'll catch it 
-    const handleChange = (event) =>{
+    const handleChange = (event) => {
         setNewTask(event.target.value);
     }
     // purana array bna hua h, pehle use pura copy kro and then last mein apne value add krdo .
-    const handleClick = () => {
+    const addTask = () => {
         const newTodoList = [...todolist, newTask];
         setTodoList(newTodoList);
     }
-    const deleteTask = () => {
-        setTodoList([]);
+    const deleteTask = (taskName) => {
+        const newTodoList = todolist.filter((task) => {
+            if (task === taskName) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        });
+        setTodoList(newTodoList);
     }
     return (
         <>
             <div className='App'>
                 <div className='addTask'>
-                    <input onChange={handleChange}/>
-                    <button onClick={handleClick}> Add task </button>
+                    <input onChange={handleChange} />
+                    <button onClick={addTask}> Add task </button>
                 </div>
                 <div className='list'>
                     {/* idhar bas data display krna tha so we're mapping the data  */}
-                    {todolist.map((task)=>{
-                            return(
-                                <div>
-                                    <h1>{task}</h1>
-                                    <button onClick={deleteTask(task)}>X</button>
-                                </div>
-                            )
+                    {todolist.map((task) => {
+                        return (
+                            <div>
+                                <h1>{task}</h1>
+                                <button onClick={deleteTask(task)}>X</button>
+                            </div>
+                        )
                     })}
                 </div>
             </div>
