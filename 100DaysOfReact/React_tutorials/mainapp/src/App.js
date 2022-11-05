1d7ce1c7d319211ccf56a2d4cd06e81c2dc53352
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-05 17:15:38 +0530

    5 november

--- 100DaysOfReact/React_tutorials/mainapp/src/App.js
+++ 100DaysOfReact/React_tutorials/mainapp/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState, createContext } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Pages
+import Home from "../src/pages/Home"
 // import Counter from './components/Counter'
 // import Counter2 from './components/Counter2'
 // import Counter3 from './components/Counter3'
@@ -12,12 +16,20 @@ import React from 'react'
 // import Crud from './crud/Crud'
 // import Effect from './hooks/Effect'
 // import Api from './Api'
-// import PredictAge from './components/PredictAge'
-// import Excuses from './components/Excuses'
+import PredictAge from './components/PredictAge'
+import Excuses from './components/Excuses'
 import Nationalize from './components/Nationalize'
 export default function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/nationality' element={<Nationalize/>} />
+          <Route path='/excuses' element={<Excuses/>} />
+          <Route path='/age' element={<PredictAge/>} />
+        </Routes>
+      </Router>
       {/* <Counter /> */}
       {/* <Counter2 /> */}
       {/* <Counter3 /> */}
@@ -33,7 +45,7 @@ export default function App() {
       {/* <Api/> */}
       {/* <PredictAge/> */}
       {/* <Excuses /> */}
-      <Nationalize/>
+      {/* <Nationalize/> */}
     </>
   )
 }
