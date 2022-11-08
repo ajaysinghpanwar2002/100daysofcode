04a089d5d2a3ed1680a44f115a0a2aa1f2fbc45b
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-08 10:46:14 +0530

    8 november

--- 100DaysOfReact/React_tutorials/typescript-app/src/App.tsx
+++ 100DaysOfReact/React_tutorials/typescript-app/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
 import './App.css';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      App
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
       </header>
     </div>
   );
---
+++ 100DaysOfReact/React_tutorials/typescript-app/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <input/>
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Login
