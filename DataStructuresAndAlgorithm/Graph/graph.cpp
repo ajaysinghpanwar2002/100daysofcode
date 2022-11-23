f5199966ce0531fcce5ca1403a75e7fd3a0d565b
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-23 10:00:47 +0530

    graphs

---
+++ DataStructuresAndAlgorithm/Graph/graph.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    // graph here using adjacency matrix
+    int adj[n + 1][m + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    // graph here using adjacency list
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return 0;
+}
